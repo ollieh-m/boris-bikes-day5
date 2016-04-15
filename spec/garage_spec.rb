1686e1e48afe1543b0c0f99e91894e74e69bc3a6
ollieh-m <ollie.hm@cantab.net>
2016-04-15 13:00:29 +0100

    Challenge 21 - test for garage fixing bikes

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,11 +1,20 @@
 require 'Garage'
 
 describe Garage do
-  let(:broken_bikes) {[double(:broken_bike1), double(:broken_bike2)]}
+  
+  let(:broken_bikes) {[spy(:broken_bike1), spy(:broken_bike2)]}
+  
   it 'receives bikes' do
     subject.receive_broken(broken_bikes)
     broken_bikes.each do |bike|
       expect(subject.bikes).to include bike
     end
   end
+
+  it 'fixes broken bikes' do
+  	subject.receive_broken(broken_bikes)
+  	subject.fix_bikes
+  	expect(broken_bikes[0]).to have_received(:fix)
+  end
+
 end
